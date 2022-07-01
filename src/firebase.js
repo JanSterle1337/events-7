6f0c19df4394030f7d53044dd779735754e6ee58
Jan Sterle <86469319+JanSterle1337@users.noreply.github.com>
2022-07-01 16:16:31 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { collection, getDoc } from 'firebase/firestore';
 
 
 const config = {                                                          //DATA FOR CONNECTION TO MY DATABASE
-    apiKey: "AIzaSyAZDtwzpOhE8dpdPeNQs5E2LlwkFNt_qHk",
-    authDomain: "crud-app-a37c2.firebaseapp.com",
-    projectId: "crud-app-a37c2",
-    storageBucket: "crud-app-a37c2.appspot.com",
-    messagingSenderId: "726472562226",
-    appId: "1:726472562226:web:ea944d26061a7cfb56ce2b",
-    measurementId: "G-TQSL8VEX52"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
 
 
